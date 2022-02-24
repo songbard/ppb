5c8657843d53fcfdcdb4f6541777fa4ff2b7b155
Jonathan Betz Brown <77259249+songbard@users.noreply.github.com>
2022-02-23 21:58:51 -0800

    updated the comments

--- create_incident_ds.R
+++ create_incident_ds.R
@@ -1,19 +1,27 @@
 newtable <- ppbforce_clean_q321[,c(35,1:9)]
-#the next line only retains rows that are not duplicated.  on every var.
+
+#the next line only retains rows that are not duplicated, on every var, that is, it picks (presumably) the first row
+#of a set of duplicated rows and drops the rest.
+  #NOTE: need to check to see how exactly it decides which rows to drop
+  #NOTE: at this point, i can sort the ds so that the first row is the one that I most want to keep.
+  #NOTE: according to Ray, rows must be exact dups on ALL variables for unique() to drop them.
+      #NOTE: this needs to be checked to see that no minor data entry errors are preventing dups from being recognized
+
 newtable <- unique(newtable)
 
 #next step is to create the new composite variables that will be needed
-#creating new vars starts with the full dataset, then "add to"merge" with the smaller "newtable"
-#but first make sure newtable is unique (no dups)
+#creating new vars starts with the full dataset, then add to"merge" with the smaller "newtable"
+#but first make sure newtable is unique (no rows that are dups but look like they are not because some value differs)
 # but this removes the last dup, does not keep both
 # this table is for investigation only, and it only removes the first of a duplicate pair or triple or...
 #PLUS all the unduplicaterd rows
-newtable_uncorrected_dups_only <- newtable[duplicated(newtable$incid_id),]  #square brackets subset the dataset, r before c, sep by comma 
+newtable_uncorrected_dups_only <- newtable[duplicated(newtable$incid_id),]  #square brackets subset the dataset, 
+  #r before c, sep by comma 
 
 #take first row for keepers
 newtable_first_dup_only <- newtable[!duplicated(newtable$incid_id),] 
-#this is a table of unique incident id with the data pulled from the first row containg that incid_id
-#cojld be ssorted later to make a more sensible cohoice of row
+#this is a table of unique incident id with the data pulled from the first row containing that incid_id
+
 
 ppbforce_clean_q321_by_incid_id  <- newtable_first_dup_only
 
